92690f78df95bf24fc9364cc0f650bbd0cda6e4f
Алексей Козярский <kozyarskii.as@phystech.edu>
2025-03-13 03:06:06 +0300

    попытался сделать градиент

--- hello_ili9341/hello_ili9341.c
+++ hello_ili9341/hello_ili9341.c
@@ -5,6 +5,8 @@
 #include "gradient/gradient.h"
 #include "stdlib.h"
 
+#define COLOR_COUNT 4 
+
 // SPI Defines
 // We are going to use SPI 0, and allocate it to the following GPIO pins
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
@@ -18,9 +20,18 @@ int main()
     init_display();
     init_drawing();
 
+
+    const uint16_t colors[] = {
+        0xF800,  // Красный
+        0x07E0,  // Зеленый
+        0x001F,  // Синий
+        0xFFFF   // Белый
+    };
+    
+
     while (true) {
         clear_buffer();
-        handle_sin_gradient(get_buffer());
+        handle_gradient(colors, COLOR_COUNT, get_buffer());
         display_buffer();
         // display_external_buffer(get_antlab_image_buffer());
     }
